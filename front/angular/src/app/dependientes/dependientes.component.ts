cef7dbe5506e03aa2d5d99dd2e160a217c894828
CarBe03 <carolopintag2003@hotmail.com>
2024-08-28 20:41:20 -0500

    carpeta dependientes

---
+++ front/angular/src/app/dependientes/dependientes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dependientes',
+  standalone: true,
+  imports: [],
+  templateUrl: './dependientes.component.html',
+  styleUrl: './dependientes.component.scss'
+})
+export class DependientesComponent {
+
+}
